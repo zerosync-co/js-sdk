8596a4cf14ad3f098df78f0f0d823ba55bb4602f
Alex Dunne <alex12dunne@gmail.com>
2024-07-10 06:50:17 -0600

    broadcast wasm output to nats

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import createPlugin from '@extism/extism'
+import { connect } from 'nats'
 
-async function runModule(path, pluginConfig, params) {
+async function runModule(
+  path,
+  subject,
+  pluginConfig,
+  params,
+  natsServers = {}
+) {
   const plugin = await createPlugin(path, pluginConfig)
-
   const input = new TextEncoder().encode(JSON.stringify(params))
-  const res = await plugin.call('_main', input)
-  return JSON.parse(new TextDecoder().decode(res.buffer))
+  const output = await plugin.call('_main', input)
+
+  const textOutput = new TextDecoder().decode(output.buffer)
+
+  const nc = await connect(natsServers)
+  nc.publish(subject, textOutput)
+  await nc.drain()
+
+  return JSON.parse(textOutput)
 }
 
 export default runModule
