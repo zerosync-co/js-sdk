417b77e7d7c532c7ca0bae52bbc6db0ac9ca6b15
Alex Dunne <alex12dunne@gmail.com>
2024-07-09 21:13:25 -0600

    runModule default export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-function helloWorld() {
-  console.log('hello world')
+import createPlugin from '@extism/extism'
+
+const baseUrl = 'https://gihub.com/zerosync-co/js-sdk/blob/main'
+
+const registry = {
+  fetch: `${baseUrl}/wasm_fetch.wasm`,
 }
 
-export default helloWorld
+async function runModule(name, pluginConfig, params) {
+  let moduleUrl = registry[name]
+  if (!moduleUrl) {
+    throw new Error(
+      `failed to resolve module from name; ${name} does not exist`
+    )
+  }
+
+  const plugin = await createPlugin(
+    {
+      url: moduleUrl,
+    },
+    {
+      ...pluginConfig,
+      useWasi: true,
+    }
+  )
+
+  const input = new TextEncoder().encode(params)
+  const res = await plugin.call('_main', input)
+  return JSON.parse(new TextDecoder().decode(res.buffer))
+}
+
+export default runModule
