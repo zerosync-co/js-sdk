26bbaf8a993accc3c28ae9343f9df4bd48ccea15
Alex Dunne <alex12dunne@gmail.com>
2024-07-09 21:33:09 -0600

    fix json serialization

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import createPlugin from '@extism/extism'
 async function runModule(path, pluginConfig, params) {
   const plugin = await createPlugin(path, pluginConfig)
 
-  const input = new TextEncoder().encode(params)
+  const input = new TextEncoder().encode(JSON.stringify(params))
   const res = await plugin.call('_main', input)
   return JSON.parse(new TextDecoder().decode(res.buffer))
 }
