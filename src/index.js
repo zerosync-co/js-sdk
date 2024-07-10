c1652fcde696f6f6c983a0b9390007357933d8c5
Alex Dunne <alex12dunne@gmail.com>
2024-07-09 21:26:36 -0600

    change to pass module path

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import createPlugin from '@extism/extism'
 
-const registry = {
-  fetch: `../wasm_fetch.wasm`,
-}
-
-async function runModule(name, pluginConfig, params) {
-  let moduleUrl = registry[name]
-  if (!moduleUrl) {
-    throw new Error(
-      `failed to resolve module from name; ${name} does not exist`
-    )
-  }
-
-  const plugin = await createPlugin(moduleUrl, {
-    ...pluginConfig,
-    useWasi: true,
-  })
+async function runModule(path, pluginConfig, params) {
+  const plugin = await createPlugin(path, pluginConfig)
 
   const input = new TextEncoder().encode(params)
   const res = await plugin.call('_main', input)
