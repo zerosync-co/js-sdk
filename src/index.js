f6b909e430e6615d60c8faee1f6628cd6785497d
Alex Dunne <alex12dunne@gmail.com>
2024-07-13 09:29:41 -0600

    requireNatsConn function, formatting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
-import createPlugin from '@extism/extism'
-import { connect } from 'nats'
+import createPlugin from "@extism/extism";
+import { connect } from "nats";
+
+let natsConn;
+
+async function requireNatsConn(natsServers) {
+  if (!natsConn) {
+    natsConn = await connect(natsServers);
+
+    console.log("successfully required nats connection");
+  }
+}
 
 async function runModule(
   path,
   subject,
   pluginConfig,
   params,
-  natsServers = {}
+  natsServers = {},
 ) {
-  const plugin = await createPlugin(path, pluginConfig)
-  const input = new TextEncoder().encode(JSON.stringify(params))
-  const output = await plugin.call('_main', input)
+  const plugin = await createPlugin(path, pluginConfig);
+  const input = new TextEncoder().encode(JSON.stringify(params));
+  const output = await plugin.call("_main", input);
 
-  const textOutput = new TextDecoder().decode(output.buffer)
+  const textOutput = new TextDecoder().decode(output.buffer);
 
-  const nc = await connect(natsServers)
-  nc.publish(subject, textOutput)
-  await nc.drain()
+  await requireNatsConn(natsServers);
+  natsConn.publish(subject, textOutput);
+  await natsConn.flush();
 
-  return JSON.parse(textOutput)
+  return JSON.parse(textOutput);
 }
 
-export default runModule
+export default runModule;
